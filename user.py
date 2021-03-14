987ba934c22460b4eda55b53d29673d387f59c13
JPatel <80221415+JPatel94@users.noreply.github.com>
2021-03-14 10:34:00 -0500

    Add files via upload

--- user.py
+++ user.py
@@ -1,10 +1,17 @@
-from .data import users
+from datetime import datetime
+from turtle import clear
+from event import EventUser
+from .data import users, event_users
 
+
+def logout():
+    exit()
 
 class User:
     counter = 0
 
-    def __init__(self, name, email, password, is_admin):
+    def __init__(self, name, email, password, is_admin, events):
+        self.events = events
         self.name = name
         self.email = email
         self.password = password
@@ -14,25 +21,45 @@ class User:
 
     @classmethod
     def login(cls, email, password):
-        pass
+        if users.email:
+            if users.password:
+                print("login successfully")
+            else:
+                print("wrong password")
+        else:
+            print("enter correct email id")
 
-    def logout(self):
-        pass
+    def join_event(self, events):
+        if self.events.id == 0 or 1:
+            events.join()
 
-    def join_event(self, event_id):
-        pass
+    def create_event(self, start_datetime=datetime, description=str, name=str):
+        for e in self.events:
+            if e['id'] == self.id:
+                e['start_datetime'] = start_datetime
+                e['description'] = description
+                e['name'] = name
+            return self.events.create(e)
 
-    def create_event(self):
-        pass
+    def get_all_hosted(self,events):
+        for el in self.events:
+            if el['is_host']:
+                return event_users.get_event_users(el['event_users'])
+            else:
+                return False
 
     def is_host(self, event_id):
-        pass
+        event_user = EventUser.get_eu(self.id, event_id)
+        if event_user['is_host']:
+            for e in self.events:
+                if e['id'] == self.id:
+                    return e
+        else:
+            return False
 
-    def get_all_hosted(self):
-        pass
-
-    def cancel_rsvp(self, event_id):
-        pass
+    def cancel_rsvp(self):
+        if self.id != 0 or 1:
+             clear()
 
     @classmethod
     def get_user(cls, user_id):
